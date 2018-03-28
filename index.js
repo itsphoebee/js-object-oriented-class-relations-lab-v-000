71ef898eaa3b9ff26f4bc2cf3657691b16c9de93
Phoebe Lau <phoebelau7@gmail.com>
2018-03-28 04:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class Driver{
   passengers(){
     const previousPassengers = []
     return store.passengers.filter(passenger => passenger.driverId === this.id
-    previousPassengers.push(this))
+    previousPassengers.push(passenger))
     return previousPassengers
   }
 }
